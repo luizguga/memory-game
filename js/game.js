e7174aeda0cdd8491ba8172aaaa77fb8ca2fb3c8
Luiz Gustavo <112131771+luizguga@users.noreply.github.com>
2023-08-02 16:38:46 -0300

    Jogo da memória parte 02

--- js/game.js
+++ js/game.js
@@ -22,18 +22,26 @@ const createElement = (tag, className) => {
 let firstCard = '';
 let secondCard = '';
 
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if(disabledCards.length === 20){
+        setTimeout(()=>{alert('Venceu!')}, 400);
+    }
+}
+
 const checkCards = () => {
     const firstCharacter = firstCard.getAttribute('data-character');
     const secondCharacter = secondCard.getAttribute('data-character');
 
     if(firstCharacter === secondCharacter){
-        setTimeout(()=> {
-            firstCard.firstChild.classList.add('disabled');
-            secondCard.firstChild.classList.add('disabled');
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
 
-            firstCard = '';
-            secondCard = '';
-        }, 400)
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
     }else{
         setTimeout(()=>{
             firstCard.classList.remove('reveal-card');
